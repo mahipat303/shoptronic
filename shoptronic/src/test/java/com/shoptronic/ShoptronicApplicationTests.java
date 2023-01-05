8e31c0a1ffcedc6ce60b0bc6a6b4362c6f177b6e
Mahipat Parmar <memahipat303@gmail.com>
2023-01-05 21:34:25 +0530

    maven project created

---
+++ shoptronic/src/test/java/com/shoptronic/ShoptronicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoptronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoptronicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
